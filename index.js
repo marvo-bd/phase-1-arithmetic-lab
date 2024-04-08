f31013595d0e3a07b64be49a685a4a51614be7a3
Marvin Kabira <marvobd@gmail.com>
2024-04-08 22:41:31 +0300

    Added code to index.js file as per the instructions.

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+// "multiply" variable is an equation whose multiplied results will equal 62
+let num1 = 2;
+let num2 = 31;
 
-// Write your code here
+let multiply = num1 * num2;
+
+// "random" variable generates a random integer greater than 0
+let random;
+ random = Math.floor(Math.random() * 10) + 1;
+
+// "mod" variable is an equation that calculates a remainder that is equal to 4
+let num3;
+let num4;
+ num3 = 39;
+ num4 = 7;
+
+let mod = num3 % num4;
+
+// "max" variable will return 20 as the highest number in the set
+let max;
+ max = Math.max(2,12,19,8,20,15);
